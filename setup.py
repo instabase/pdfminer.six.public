cb860640b0e5ae9e2af754ff1228a4af0c0e60ca
Lenny Khazan <lenny.khazan@gmail.com>
2022-12-10 18:02:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), "README.md")) as f:
 
 setup(
     name="pdfminer.six",
-    version=package.__version__,
+    version="20221210",
     packages=["pdfminer"],
     package_data={"pdfminer": ["cmap/*.pickle.gz", "py.typed"]},
     install_requires=[
